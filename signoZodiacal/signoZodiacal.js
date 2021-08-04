fedb187b6982e215d66a6a05cfac14fdc75d112f
mjcald <mjcald@gmail.com>
2021-08-04 12:46:55 -0600

    se modifica método signoZodiacal a uno mas eficiente y fácil comprensión

--- signoZodiacal/signoZodiacal.js
+++ signoZodiacal/signoZodiacal.js
@@ -1,99 +1,71 @@
 /**
  * @param {Date} fecha
  */
-function signoZodiacal(fecha) {
-    // Aries 21 de marzo - 20 de abril
-    let fecha1 = new Date(fecha.getFullYear(), 3-1, 21) // crea nueva fecha en base al año de la fecha proporcionada
-    //console.log(fecha1)
-    let fecha2 = new Date(fecha.getFullYear(), 4-1, 20)
-    if (fecha>=fecha1 && fecha<=fecha2) {
-        return 'Aries'
-    } else {
-        // Tauro 21 de abril - 20 de mayo
-        fecha1 = new Date(fecha.getFullYear(), 4-1, 21)
-        fecha2 = new Date(fecha.getFullYear(), 5-1, 20)
-        if (fecha>=fecha1 && fecha<=fecha2) {
-            return 'Tauro'
-        } else {
-            // Géminis 21 de mayo - 21 de junio
-            fecha1 = new Date(fecha.getFullYear(), 5-1, 21)
-            fecha2 = new Date(fecha.getFullYear(), 6-1, 21)
-            if (fecha>=fecha1 && fecha<=fecha2) {
+function signoZodiacalv2(fecha) {
+    let dia = fecha.getDate()
+    let mes = fecha.getMonth()+1 // Enero = 0, Febrero = 1, ...
+    
+    switch (mes) {
+        case 1:
+            if (dia>=21)
+                return 'Acuario'
+            else
+                return 'Capricornio'
+        case 2:
+            if (dia>=19)
+                return 'Piscis'
+            else
+                return 'Acuario'
+        case 3:
+            if (dia>=20)
+                return 'Aries'
+            else
+                return 'Piscis'
+        case 4:
+            if (dia>=21)
+                return 'Tauro'
+            else
+                return 'Aries'
+        case 5:
+            if (dia>=21)
                 return 'Géminis'
-            } else {
-                // Cáncer 22 de junio - 22 de julio
-                fecha1 = new Date(fecha.getFullYear(), 6-1, 22)
-                fecha2 = new Date(fecha.getFullYear(), 7-1, 22)
-                if (fecha>=fecha1 && fecha<=fecha2) {
-                    return 'Cáncer'
-                } else {
-                    // Leo 23 de julio - 22 de agosto
-                    fecha1 = new Date(fecha.getFullYear(), 7-1, 23)
-                    fecha2 = new Date(fecha.getFullYear(), 8-1, 22)
-                    if (fecha>=fecha1 && fecha<=fecha2) {
-                        return 'Leo'
-                    } else {
-                        // Virgo 23 de agosto - 22 de septiembre
-                        fecha1 = new Date(fecha.getFullYear(), 8-1, 23)
-                        fecha2 = new Date(fecha.getFullYear(), 9-1, 22)
-                        if (fecha>=fecha1 && fecha<=fecha2) {
-                            return 'Virgo'
-                        } else {
-                            // Libra 23 de septiembre - 22 de octubre
-                            fecha1 = new Date(fecha.getFullYear(), 9-1, 23)
-                            fecha2 = new Date(fecha.getFullYear(), 10-1, 22)
-                            if (fecha>=fecha1 && fecha<=fecha2) {
-                                return 'Libra'
-                            } else {
-                                // Escorpión 23 de octubre - 22 de noviembre
-                                fecha1 = new Date(fecha.getFullYear(), 10-1, 23)
-                                fecha2 = new Date(fecha.getFullYear(), 11-1, 22)
-                                if (fecha>=fecha1 && fecha<=fecha2) {
-                                    return 'Escorpión'
-                                } else {
-                                    // Sagitario 23 de noviembre - 21 de diciembre
-                                    fecha1 = new Date(fecha.getFullYear(), 11-1, 23)
-                                    fecha2 = new Date(fecha.getFullYear(), 12-1, 21)
-                                    if (fecha>=fecha1 && fecha<=fecha2) {
-                                        return 'Sagitario'
-                                    } else {
-                                        // Capricornio 22 de diciembre - 20 de enero
-                                        fecha1 = new Date(fecha.getFullYear(), 12-1, 22)
-                                        fecha2 = new Date(fecha.getFullYear()+1, 1-1, 20)
-                                        if (fecha>=fecha1 && fecha<=fecha2) {
-                                            return 'Capricornio'
-                                        } else {
-                                            // Capricornio 22 de diciembre - 20 de enero
-                                            fecha1 = new Date(fecha.getFullYear()-1, 12-1, 22)
-                                            fecha2 = new Date(fecha.getFullYear(), 1-1, 20)
-                                            if (fecha>=fecha1 && fecha<=fecha2) {
-                                                return 'Capricornio'
-                                            } else {
-                                                // Acuario 21 de enero - 18 de febrero
-                                                fecha1 = new Date(fecha.getFullYear(), 1-1, 21)
-                                                fecha2 = new Date(fecha.getFullYear(), 2-1, 18)
-                                                if (fecha>=fecha1 && fecha<=fecha2) {
-                                                    return 'Acuario'
-                                                } else {
-                                                    // Piscis 19 de febrero - 20 de marzo
-                                                    fecha1 = new Date(fecha.getFullYear(), 1-1, 21)
-                                                    fecha2 = new Date(fecha.getFullYear(), 2-1, 18)
-                                                    if (fecha>=fecha1 && fecha<=fecha2) {
-                                                        return 'Piscis'
-                                                    } else {
-                                                        return 'ninguno'
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+            else
+                return 'Tauro'
+        case 6:
+            if (dia>=22)
+                return 'Cáncer'
+            else
+                return 'Géminis'
+        case 7:
+            if (dia>=23)
+                return 'Leo'
+            else
+                return 'Cáncer'
+        case 8:
+            if (dia>=23)
+                return 'Virgo'
+            else
+                return 'Leo'
+        case 9:
+            if (dia>=23)
+                return 'Libra'
+            else
+                return 'Virgo'
+        case 10:
+            if (dia>=23)
+                return 'Escorpio'
+            else
+                return 'Libra'
+        case 11:
+            if (dia>=23)
+                return 'Sagitario'
+            else
+                return 'Escorpio'
+        case 12:
+            if (dia>=22)
+                return 'Capricornio'
+            else
+                return 'Sagitario'
     }
 }
 
@@ -133,7 +105,7 @@ function obtenerDatos(fechaE = '') {
     if (!isNaN(fecha2)) {
         let dv = diasVivo(fecha2)
         if (dv >= 0) {
-            resultado.innerHTML = '<br />Tu signo zodiacal es <b>' + signoZodiacal(fecha2) + '</b><br />'
+            resultado.innerHTML = '<br />Tu signo zodiacal es <b>' + signoZodiacalv2(fecha2) + '</b><br />'
             resultado.innerHTML += 'Naciste un dia <b>' + diaNacimiento(fecha2) + '</b><br />'
             resultado.innerHTML += 'Has vivido <b>' + dv + '</b> dias <br />'
             resultado.innerHTML += 'La fecha de hoy es ' + new Date().toLocaleDateString() + '<br />'
